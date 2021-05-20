6a0e0dbeb96a8df4e3ed52c657691c1199d70756
albertrodtab <albertrodtab@gmail.com>
2021-05-20 19:55:50 +0200

    Se crea la rama usuario, se crea la clase usuario con sus atributos y métodos y se sube al repositorio.

---
+++ src/main/java/domain/Usuario.java
@@ -0,0 +1,47 @@
+package domain;
+
+public class Usuario {
+    String nombre;
+    String apellidos;
+    String email;
+    String contraseña;
+
+    public Usuario(String nombre, String apellidos, String email, String contraseña) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
