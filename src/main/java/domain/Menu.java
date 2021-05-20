7140f66f0cf686e26c08d9fc5547e96871baa7c7
albertrodtab <albertrodtab@gmail.com>
2021-05-20 20:16:45 +0200

    Se crea la rama menú y se crea la clase menú con sus atributos y métodos.

---
+++ src/main/java/domain/Menu.java
@@ -0,0 +1,47 @@
+package domain;
+
+public class Menu {
+    String nombre;
+    String descripcion;
+    float precio;
+    boolean disponible;
+
+    public Menu(String nombre, String descripcion, float precio, boolean disponible) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.disponible = disponible;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+}
